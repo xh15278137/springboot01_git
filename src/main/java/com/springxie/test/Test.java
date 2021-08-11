1193c1aa6df9e8266dd9444d520d54363d775818
xh_glb <goodxh_glb>
2021-08-11 14:38:31 +0800

    insert 加载

--- src/main/java/com/springxie/test/Test.java
+++ src/main/java/com/springxie/test/Test.java
@@ -3,6 +3,7 @@ package com.springxie.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("加载");
 
     }
 }
