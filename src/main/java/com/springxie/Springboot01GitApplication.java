d601fa8aa324e8b404f0bfd5c04df46d7a800269
xh_glb <goodxh_glb>
2021-08-11 14:35:03 +0800

    new springboot01_git

---
+++ src/main/java/com/springxie/Springboot01GitApplication.java
@@ -0,0 +1,13 @@
+package com.springxie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01GitApplication.class, args);
+    }
+
+}
